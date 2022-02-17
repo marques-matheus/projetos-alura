63ef2230c959af5f5b1d9ca6f320155b2f6ec53c
marques-matheus <marques.m4th3u5@gmail.com>
2022-02-17 19:33:28 -0300

    finalização do projeto

---
+++ React/formulario/src/App.js
@@ -0,0 +1,31 @@
+import { Container, Typography } from "@material-ui/core";
+import React from "react";
+import "./App.css";
+import "@fontsource/roboto";
+
+import FormularioCadastro from "./components/Formulario/FormularioCadastro";
+
+function App() {
+  return (
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" align="center" component="h1">
+        Formulário de Cadastro
+      </Typography>
+      <FormularioCadastro onSubmit={onSubmit} cpfValido={cpfValido} />
+    </Container>
+  );
+}
+
+function onSubmit(dados) {
+  console.log(dados);
+}
+
+function cpfValido(cpf) {
+  if (cpf.length !== 11) {
+    return { valido: false, texto: "CPF dete ter 11 digitos" };
+  } else{
+    return {valido: true, texto: ''}
+  }
+}
+
+export default App;
