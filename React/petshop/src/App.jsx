a7095ec12e065dfc24b4a928387ce20181f51aa2
marques-matheus <marques.m4th3u5@gmail.com>
2022-02-26 21:17:11 -0300

    Adição da filtragem de categorias

--- React/petshop/src/App.jsx
+++ React/petshop/src/App.jsx
@@ -6,6 +6,7 @@ import Sobre from "./paginas/Sobre";
 import Cabecalho from "./components/cabecalho";
 import Pagina404 from "./paginas/pagina404";
 import Post from "./paginas/post";
+import Categoria from "./paginas/Categoria";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route path="/sobre">
           <Sobre />
+        </Route>
+        <Route path="/categoria/:id">
+          <Categoria />
         </Route>
         <Route path="/posts/:id">
           <Post />
