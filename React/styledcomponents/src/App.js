151a25a4eea8cf530ad09fdc5c7a4c1f6e9a550d
marques-matheus <marques.m4th3u5@gmail.com>
2022-03-10 22:01:03 -0300

    Adição do tema escuro e finalização do projeto

--- React/styledcomponents/src/App.js
+++ React/styledcomponents/src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/Temas";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { BtnTema } from "./Components/UI";
+import SwitcherTemas from "./Components/SwitcherTemas";
 
 function App() {
+  const [tema, setTema] = useState(true);
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  };
   return (
-    <>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTemas tema={tema} />
+      </BtnTema>
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
