fa238fb1cd4ef5fa8461be4bae48494d87472fe4
marques-matheus <marques.m4th3u5@gmail.com>
2022-03-28 20:08:25 -0300

    Progamação defensiva no atirbuto Data

--- TypeScript/app/models/negociacao.ts
+++ TypeScript/app/models/negociacao.ts
@@ -1,22 +1,14 @@
 export class Negociacao {
-  private _data: Date;
-  private _quantidade: number;
-  private _valor: number;
-  constructor(data: Date, quantidade: number, valor: number) {
-    this._data = data;
-    this._quantidade = quantidade;
-    this._valor = valor;
-  }
-  get data(): Date {
-    return this._data;
-  }
-  get quantidade(): number {
-    return this._quantidade;
-  }
-  get valor(): number {
-    return this._valor;
-  }
+  constructor(
+    private _data: Date,
+    public readonly _quantidade: number,
+    public readonly _valor: number
+  ) {}
 
+  get data(): Date {
+    const data = new Date(this._data.getTime());
+    return data;
+  }
   get volume(): number {
     return this._quantidade * this._valor;
   }
