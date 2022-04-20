99b628c98923905915df95c5bda986f59716e768
Matheus Marques <marques.m4th3u5@gmail.com>
2022-04-20 19:47:41 -0300

    Refatoração da Classe View para classe Abstrata

--- TypeScript/app/views/view.ts
+++ TypeScript/app/views/view.ts
@@ -1,12 +1,11 @@
-export class View<T> {
+export abstract class View<T> {
   protected elemento: HTMLElement;
 
   constructor(seletor: string) {
     this.elemento = document.querySelector(seletor);
   }
-  template(model: T): string {
-    throw Error("Classe filha precisa implementar o metodo Template");
-  }
+  abstract template(model: T): string;
+  
   update(model: T): void {
     const template = this.template(model);
     this.elemento.innerHTML = template;
